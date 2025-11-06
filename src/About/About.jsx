fb63e7fab9753fa072fbc605cd60a2f70249eecd
Alvaro Quiroz <alvaroquiroz159357@gmail.com>
2025-11-06 12:17:03 -0500

    funcional

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <h2 className="section-title">Sobre mí</h2>
         <div className="about-content">
           <p>
-            Soy un desarrollador Full Stack SSR de 24 años, con más de 4 años de experiencia en desarrollo de software. Me especializo en diseñar y construir aplicaciones web robustas, escalables y eficientes, tanto en backend como en frontend.
+            Soy un desarrollador Full Stack SSR de 24 años, con más de 6 años de experiencia en desarrollo de software. Me especializo en diseñar y construir aplicaciones web robustas, escalables y eficientes, tanto en backend como en frontend.
             Como profesional autodidacta, me apasiona la mejora continua, la automatización de procesos y el trabajo colaborativo para lograr resultados de alto impacto. Disfruto asumir nuevos desafíos que impulsen mi crecimiento profesional y aporten un valor significativo a los proyectos en los que participo.
           </p>
         </div>
